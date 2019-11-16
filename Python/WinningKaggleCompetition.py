a8e06fda94ec40dee3e499880210c47805a99f79
emsinko <44008559+emsinko@users.noreply.github.com>
2019-11-16 22:02:40 +0100

    Update WinningKaggleCompetition.py

--- Python/WinningKaggleCompetition.py
+++ Python/WinningKaggleCompetition.py
@@ -843,3 +843,166 @@ rental_listings[['building_id']] = constant_imputer.fit_transform(rental_listing
 #################
 
 # Create baseline models
+
+## Baseline model
+
+#1) Predict average fare_amount from train to all test data 
+
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+# Calculate the mean fare_amount on the validation_train data
+naive_prediction = np.mean(validation_train.fare_amount)
+
+# Assign naive prediction to all the holdout observations
+validation_test['pred'] = naive_prediction
+
+# Measure the local RMSE
+rmse = sqrt(mean_squared_error(validation_test['fare_amount'], validation_test['pred']))
+print('Validation RMSE for Baseline I model: {:.3f}'.format(rmse))
+
+####
+## Baseline based on the date
+
+# The first model is based on the grouping variables. 
+# It's clear that the ride fare could depend on the part of the day. 
+# For example, prices could be higher during the rush hours.
+# Your goal is to build a baseline model that will assign the average "fare_amount" for the corresponding hour. For now, you will create the model for the whole train data and make predictions for the test dataset.
+# The train and test DataFrames are available in your workspace. Moreover, the "pickup_datetime" column in both DataFrames is already converted to a datetime object for you.
+
+# Get pickup hour from the pickup_datetime column
+train['hour'] = train['pickup_datetime'].dt.hour
+test['hour'] = test['pickup_datetime'].dt.hour
+
+# Calculate average fare_amount grouped by pickup hour 
+hour_groups = train.groupby('hour')['fare_amount'].mean()
+
+# Make predictions on the test set
+test['fare_amount'] = test.hour.map(hour_groups)
+
+# Write predictions
+test[['id','fare_amount']].to_csv('hour_mean_sub.csv', index=False)
+
+
+####
+## Baseline based on the gradient boosting
+
+# Let's build a final baseline based on the Random Forest. 
+# You've seen a huge score improvement moving from the grouping baseline to the Gradient Boosting in the video. 
+# Now, you will use sklearn's Random Forest to further improve this score.
+
+# The goal of this exercise is to take numeric features and train a Random Forest model without any tuning. 
+# After that, you could make test predictions and validate the result on the Public Leaderboard. 
+# Note that you've already got an "hour" feature which could also be used as an input to the model.
+
+from sklearn.ensemble import RandomForestRegressor
+
+# Select only numeric features
+features = ['pickup_longitude', 'pickup_latitude', 'dropoff_longitude',
+            'dropoff_latitude', 'passenger_count', 'hour']
+
+# Train a Random Forest model
+rf = RandomForestRegressor()
+rf.fit(train[features], train.fare_amount)
+
+# Make predictions on the test data
+test['fare_amount'] = rf.predict(test[features])
+
+# Write predictions
+test[['id','fare_amount']].to_csv('rf_sub.csv', index=False)
+
+
+## Next, add some new features (hour, distance..) and looking if our RMSE decrease.
+## It is a good practice to write down all validation scores and rankings with each type of model.
+## Unfortunately, you have only limited number of submissions
+
+## MUST TO DO: FEATURE ENGINEERING UNTIL WE ARE OUT OF IDEAS!! AFTER THAT, WE MOVE ON TO HYPERPARAMETER OPTIMIZATION
+
+## On the other hand, in deep learning competitions with text or images data, there is no need to feature engineering  
+## NEURAL NETS are generating features on thei own, while we need to specify the architecture and list of hyperparameters
+
+#####
+## Grid search
+
+# Recall that we've created a baseline Gradient Boosting model in the previous lesson. Your goal now is to find the best max_depth hyperparameter value for this Gradient Boosting model. 
+# This hyperparameter limits the number of nodes in each individual tree. 
+# You will be using K-fold cross-validation to measure the local performance of the model for each hyperparameter value.
+
+# You're given a function get_cv_score(), which takes the train dataset and dictionary of the 
+# model parameters as arguments and returns the overall validation RMSE score over 3-fold cross-validation.
+
+# Cez toto zistime source kod funckie
+import inspect
+lines = inspect.getsource(nazov_funkcie)
+print(lines)
+
+def get_cv_score(train, params):
+    # Create KFold object
+    kf = KFold(n_splits=3, shuffle=True, random_state=123)
+
+    rmse_scores = []
+    
+    # Loop through each split
+    for train_index, test_index in kf.split(train):
+        cv_train, cv_test = train.iloc[train_index], train.iloc[test_index]
+    
+        # Train a Gradient Boosting model
+        gb = GradientBoostingRegressor(random_state=123, **params).fit(cv_train[features], cv_train.fare_amount)
+    
+        # Make predictions on the test data
+        pred = gb.predict(cv_test[features])
+    
+        fold_score = np.sqrt(mean_squared_error(cv_test['fare_amount'], pred))
+        rmse_scores.append(fold_score)
+    
+    return np.round(np.mean(rmse_scores) + np.std(rmse_scores), 5)
+
+
+# Possible max depth values
+max_depth_grid = [3,6,9,12,15]
+results = {}
+
+# For each value in the grid
+for max_depth_candidate in max_depth_grid:
+    # Specify parameters for the model
+    params = {'max_depth': max_depth_candidate}
+
+    # Calculate validation score for a particular hyperparameter
+    validation_score = get_cv_score(train, params)
+
+    # Save the results for each max depth value
+    results[max_depth_candidate] = validation_score   
+print(results)
+
+####
+## 2D grid search
+
+# The drawback of tuning each hyperparameter independently is a potential dependency between different hyperparameters. 
+# The better approach is to try all the possible hyperparameter combinations. 
+# However, in such cases, the grid search space is rapidly expanding. 
+# For example, if we have 2 parameters with 10 possible values, it will yield 100 experiment runs.
+
+# Your goal is to find the best hyperparameter couple of max_depth and subsample for the  Gradient Boosting model.
+# Subsample is a fraction of observations to be used for fitting the individual trees.
+
+# You're given a function get_cv_score(), which takes the train dataset and dictionary of the model parameters as arguments and returns the overall validation RMSE score over 3-fold cross-validation.
+
+import itertools
+
+# Hyperparameter grids
+max_depth_grid = [3,5,7]
+subsample_grid = [0.8, 0.9, 1.0]
+results = {}
+
+# For each couple in the grid
+for max_depth_candidate, subsample_candidate in itertools.product(max_depth_grid, subsample_grid):
+    params = {'max_depth': max_depth_candidate,
+              'subsample': subsample_candidate}
+    validation_score = get_cv_score(train, params)
+    # Save the results for each couple
+    results[(max_depth_candidate, subsample_candidate)] = validation_score   
+print(results)
+
+# Do not spend too much time on the hyperparameter tuning at the beginning of the competition! 
+# Another approach that almost always improves your solution is model ensembling. Go on for it
